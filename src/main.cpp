9eafbb46809f6344375774f985591100420d1f28
tessarivas <rivas.gomez.teresa@gmail.com>
2024-05-20 00:29:31 -0700

    Cambios de tere a las 12:29 pm el domingo jojo

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include <iostream>
 #include <string>
+#include <cmath>
 
 #include "classes/Usuario.h"
 
 using std::cout;
 using std::endl;
 using std::string;
-
 
 // DEFINICIONES
 #define ANCHO 414
@@ -20,23 +19,23 @@ using std::string;
 //---------------------Escenarios------------------------------------------------
 typedef enum Pantalla{
     INICIO = 0,
-    CREAR_DUENO, // Donde se crea al dueño al entrar
+    CREAR_DUENO, 
     MIS_MASCOTAS,
     CREAR_MASCOTA,
-    /*
-    DUEÑO, // Perfil del dueño ya creado
-    CREAR_MASCOTA,
-    MASCOTA,
-    INFO_MASCOTA,
-    CARTILLAS_MASCOTA,
-    CALENDARIO_MASCOTA
-    */
 } Pantalla;
 
 //----------------------Images----------------------------------------------------
 typedef struct Cargas{
+    // INICIO
     Texture2D FondoInicio;
-    Vector2 Position;
+    Texture2D BotonEntrar;
+    // CREAR DUEÑO
+    Texture2D FondoCrearDueno;
+    // MIS MASCOTAS
+    Texture2D FondoMisMascotas;
+    // AÑADIR MASCOTAS
+    Texture2D FondoAnadirMascotas;
+    Vector2 Position = { 0.0f, 0.0f };
 } Cargas;
 
 //------------------Load And Unload Content----------------------------------------
@@ -58,7 +57,6 @@ int main(){
     // Variables
     srand(time(NULL));
     Usuario user;
-
     string tempName;
 
     // Ventana
@@ -69,56 +67,54 @@ int main(){
 
     // Iniciar pantallas
     Pantalla pantalla_actual = INICIO;
-    Cargas img; // Fondo
-    img = CargarContenido(pantalla_actual, img);
+    Cargas fondo_actual;
+    fondo_actual = CargarContenido(pantalla_actual, fondo_actual);
+    Cargas archivos;
 
     // PROGRAMA PRINCIPAL
     while(!WindowShouldClose()){
         BeginDrawing();
+        int boton_click_inicio = DibujarInicio(archivos); // Solo cuando se de click al boton y no en cualquier parte de la pantalla
+
         switch(pantalla_actual)
         {
             case INICIO:
             {
-                DibujarInicio(img);
-                if(IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-                    // Primero descargar la pantalla actual
-                    DescargarContenido(pantalla_actual, img); //En vez de tener que enviar una cadena podemos solamente enviar la pantalla
-                    // Actualizamos la variable que representa la pantalla
+                DibujarInicio(fondo_actual);
+                if(IsKeyPressed(KEY_SPACE) || boton_click_inicio == 1){
+                    DescargarContenido(INICIO, fondo_actual);
                     pantalla_actual = CREAR_DUENO;
-                    // Actualizamos la textura
-                    img = CargarContenido(pantalla_actual, img);
+                    fondo_actual = CargarContenido(pantalla_actual, fondo_actual);
                 }
-                
                 break;
             }
             case CREAR_DUENO:
             {
-                tempName = DibujarCrearPerfil(img,ANCHO,ALTO);
+                tempName = DibujarCrearPerfil(fondo_actual,ANCHO,ALTO);
                 user.DefineName(tempName);
                 if(tempName != " "){
                     user.GetName();
-                    DescargarContenido(pantalla_actual, img);
+                    DescargarContenido(CREAR_DUENO, fondo_actual);
                     pantalla_actual = MIS_MASCOTAS;
-                    img = CargarContenido(pantalla_actual, img);
+                    fondo_actual = CargarContenido(pantalla_actual, fondo_actual);
                 }
                 break;
             }
             case MIS_MASCOTAS:
             {
-                DibujarMisMascotas(img);
-                DescargarContenido(pantalla_actual, img);
+                DibujarMisMascotas(fondo_actual);
+                DescargarContenido(MIS_MASCOTAS, fondo_actual);
                 pantalla_actual = CREAR_MASCOTA;
-                img = CargarContenido(pantalla_actual, img);
+                fondo_actual = CargarContenido(pantalla_actual, fondo_actual);
                 break;
             }
             case CREAR_MASCOTA:
             {
                 cout<<"ENTRO AQUI"<<endl;
-                DibujarCrearMascota(img);
+                DibujarCrearMascota(fondo_actual);
                 if(IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
-                    DescargarContenido(pantalla_actual, img);
                     pantalla_actual = CREAR_MASCOTA;
-                    img = CargarContenido(pantalla_actual, img);
+                    fondo_actual = CargarContenido(pantalla_actual, fondo_actual);
                 }
                 break;
             }
@@ -127,14 +123,12 @@ int main(){
                 break;
         }
         EndDrawing();
-        // DibujarMenu(pantalla_actual, archivos);
     }
 
     // DESCARGAR CONTENIDO
-    DescargarContenido(pantalla_actual, img);
+    DescargarContenido(pantalla_actual, fondo_actual);
     UnloadImage(icono);
 
-    // CERRAR VENTANA
     CloseWindow();
 
     return 0;
@@ -142,67 +136,79 @@ int main(){
 
 // CARGAR TODAS LAS IMAGENES, AUDIO,FONTS, ETC...
 Cargas CargarContenido(Pantalla actual, Cargas archivos){
-    
     switch (actual)
     {
         case INICIO:
         {
             archivos.FondoInicio = LoadTexture("../assets/PetCare_PantallaInicio.png");
-            archivos.Position.x=0;
-            archivos.Position.y=0;
+            archivos.BotonEntrar = LoadTexture("../assets/PetCare_BotonEntrar.png");
             break;
         }
         case CREAR_DUENO:
         {
-            archivos.FondoInicio = LoadTexture("../assets/CrearDuenoPreview.png");
+            archivos.FondoCrearDueno = LoadTexture("../assets/PetCare_CrearDueno.png");
             break;
         }
         case MIS_MASCOTAS:
         {
-            archivos.FondoInicio=LoadTexture("../assets/MisMascotas.png");
+            archivos.FondoMisMascotas=LoadTexture("../assets/PetCare_MisMascotas.png");
             break;
         }
         case CREAR_MASCOTA:
         {
-            archivos.FondoInicio=LoadTexture("../assets/AnadirMascota.png");
+            archivos.FondoAnadirMascotas=LoadTexture("../assets/PetCare_AnadirMascota.png");
             break;
         }
         default:
         break;
     }
-
     return archivos;
 }
 
-// DESCARGAR TODAS LAS IMAGENES, AUDIO,FONTS, ETC...
+// DESCARGAR TODAS LAS IMAGENES, AUDIO, FONTS, ETC...
 void DescargarContenido(Pantalla pantalla_actual, Cargas archivos){
-    UnloadTexture(archivos.FondoInicio);
+    if(pantalla_actual == INICIO){
+        UnloadTexture(archivos.FondoInicio);
+        UnloadTexture(archivos.BotonEntrar);
+    }
+    if(pantalla_actual == CREAR_DUENO){
+        UnloadTexture(archivos.FondoCrearDueno);
+    }
+    if(pantalla_actual == MIS_MASCOTAS){
+        UnloadTexture(archivos.FondoMisMascotas);
+    }
+    if(pantalla_actual == CREAR_MASCOTA){
+        UnloadTexture(archivos.FondoAnadirMascotas);
+    }
 }
 
 // DIBUJAR LA PANTALLA DE INICIO CON LA QUE ENTRARA
-int DibujarInicio(Cargas archivos){
-    // DrawTexturePro(
-    //     archivos.FondoInicio,
-    //     (Rectangle){0, 0, (float)archivos.FondoInicio.width, (float)archivos.FondoInicio.height},
-    //     (Rectangle){0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()},
-    //     (Vector2){0, 0},
-    //     0.0f,
-    //     WHITE);
-    DrawTextureEx(archivos.FondoInicio,archivos.Position,0.0f,1.0f,WHITE);
-    
+int DibujarInicio(Cargas archivos) {
+    // FONDO DE INICIO
+    DrawTextureEx(archivos.FondoInicio, archivos.Position, 0.0f, 1.0f, WHITE);
+
+    // ------------------- BOTON "ENTRAR" -------------------
+    // Tamaño del boton
+    float boton_entrar_width = static_cast<float>(archivos.BotonEntrar.width);
+    float boton_entrar_height = static_cast<float>(archivos.BotonEntrar.height);
+    // Posición del boton centrado en la parte inferior de la pantalla
+    float boton_entrar_x = (GetScreenWidth() - boton_entrar_width) / 2;
+    float boton_entrar_y = GetScreenHeight() - boton_entrar_height - 30; 
+    // Dibujar el boton
+    DrawTexture(archivos.BotonEntrar, boton_entrar_x, boton_entrar_y, WHITE);
+    // Verificar si se presiona el boton
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) && 
+        CheckCollisionPointRec(GetMousePosition(), { boton_entrar_x, boton_entrar_y, boton_entrar_width, boton_entrar_height })) {
+        return 1;
+    }
+
     return 0;
 }
 
 // DIBUJAR LA PANTALLA DE CREAR DUEÑO
 int DibujarCrearDueno(Cargas archivos){
-    // DrawTexturePro(
-    //     archivos.FondoInicio,
-    //     (Rectangle){0, 0, (float)archivos.FondoInicio.width, (float)archivos.FondoInicio.height},
-    //     (Rectangle){0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()},
-    //     (Vector2){0, 0},
-    //     0.0f,        `
-    //     WHITE);
-    DrawTextureEx(archivos.FondoInicio,archivos.Position,0.0f,1.0f,WHITE);
+
+    DrawTextureEx(archivos.FondoCrearDueno,archivos.Position,0.0f,1.0f,WHITE);
 
     return 0;
 }
@@ -216,19 +222,22 @@ string DibujarCrearPerfil(Cargas archivos,int screenWeidth,int screenHeight){
     do
     {
         BeginDrawing();
-        DrawTextureEx(archivos.FondoInicio,archivos.Position,0.0f,1.6f,WHITE);
+        DrawTextureEx(archivos.FondoCrearDueno,archivos.Position,0.0f,1.0f,WHITE);
         ClearBackground(WHITE);
-        DrawRectangle(50.0f,320.0f,320.0f,70.0f,GRAY);
+
+        int tamano_fondo = GetScreenWidth();
+        float rectX = (tamano_fondo - 320.0f) / 2;
+        Color color_fondo_texto = { 251, 247, 235, 255 };
+        DrawRectangle(rectX, 320.0f, 320.0f, 70.0f, color_fondo_texto); // RECTANGULO NOMBRE
+        DrawRectangle(rectX, 430.0f, 320.0f, 300.0f, color_fondo_texto); // RECTANGULO AVATAR
         DrawText(name,55,325,30,BLACK);
         
         int key = GetCharPressed();
 
-        // Verificar si hay presiona mas de 1 caracter en el mismo frame
-        // Saldra del ciclo si no presiona nada
+        // Verificar si hay presiona mas de 1 caracter en el mismo frame. Saldra del ciclo si no presiona nada
         while (key > 0)
         {
-            // Solo caracteres entre el 32 y 125
-            if ((key >= 32) && (key <= 125) && (CharacterCont <MaxCharacter))
+            if ((key >= 32) && (key <= 125) && (CharacterCont <MaxCharacter)) // Solo caracteres entre el 32 y 125
             {
                 name[CharacterCont] = (char)key; //transformar el caracter de codigo asci a caracter
                 name[CharacterCont+1] = '\0'; //Agregar caracter nulo al final de la cadena
@@ -240,13 +249,12 @@ string DibujarCrearPerfil(Cargas archivos,int screenWeidth,int screenHeight){
 
         if (IsKeyPressed(KEY_BACKSPACE)) //Borrar
         {
-            // Si la cadena esta vacia no hara nada
             if (CharacterCont < 0){
-                // No hace nada
+                // Si la cadena esta vacia no hara nada
             }
-            else //Si no esta vacia eliminara un espacio
+            else
             {
-                CharacterCont--;     
+                CharacterCont--; //Si no esta vacia eliminara un espacio     
             }
             name[CharacterCont] = '\0'; //Al ultimo que antes era una letra la sustituimos por el caracter nulo
         }
@@ -265,23 +273,15 @@ void DibujarMisMascotas(Cargas archivos){
     {
         BeginDrawing();
         ClearBackground(WHITE);
-        DrawTextureEx(archivos.FondoInicio,archivos.Position,0.0f,1.2f,WHITE);
+        DrawTextureEx(archivos.FondoMisMascotas,archivos.Position,0.0f,1.0f,WHITE);
         if(IsKeyPressed(KEY_SPACE) || IsMouseButtonPressed(MOUSE_BUTTON_LEFT)){
             break;
         }
         EndDrawing();
     }
-    
 }
 
 void DibujarCrearMascota(Cargas archivos){
-    
-    // DrawTexturePro(archivos.FondoInicio,
-    //     (Rectangle){0, 0, (float)archivos.FondoInicio.width, (float)archivos.FondoInicio.height},
-    //     (Rectangle){0, 0, (float)GetScreenWidth(), (float)GetScreenHeight()},
-    //     (Vector2){0, 0},
-    //     0.0f,
-    // WHITE);
-    DrawTextureEx(archivos.FondoInicio,archivos.Position,0.0f,1.0f,WHITE);
+    DrawTextureEx(archivos.FondoMisMascotas,archivos.Position,0.0f,1.0f,WHITE);
 }
 
