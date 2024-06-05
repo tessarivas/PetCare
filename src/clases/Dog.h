b076b891584be7d2107c8bc34a8df907ddba1ebe
AdanCC21 <adangzcs.gemial@gmail.com>
2024-06-05 09:03:38 -0700

    src actualizado

---
+++ src/clases/Dog.h
@@ -0,0 +1,171 @@
+#pragma once
+#include "Usuario.h"
+#include "Eventos.h"
+#include "Citas.h"
+
+class Dog
+{
+public:
+    // Datos basicos
+    string Nombre;
+    string Raza;
+    
+    // Fecha de nacimiento
+    int Dia;
+    int Mes;
+    int Anio;
+    
+    //Datos medicos
+    float Peso;
+    string Padecimientos;
+    Texture2D Avatar;
+
+    Dog *next, *prev;
+    struct Evento *event;
+    struct Cita *date;
+    //Los padecimientos podemos hacerlo en una estructura de datos para poder poner mas de uno, pero eso ya despues :b
+
+    //Constructor
+    Dog();
+    Dog(string name, string raza, int dia, int mes, int anio,float peso, string Padecimientos);
+
+    // Gets
+    string getName();
+    string getRaza();
+    // int getDia();
+    // int getMonth();
+    // int getYear();
+    // float getPeso();
+    // string getPadecimientos();
+
+    // Define
+    void DefineAvatar(Texture2D textura);
+
+    // --- Lista --- //
+    Dog *createNodo(string name, string raza, int dia, int mes , int anio, float peso, string padecimientos,Texture2D avatar);
+    void add(Dog *&lista, string name, string raza, int dia, int mes , int anio, float peso, string padecimientos,Texture2D avatar);
+    void DefineEvents(struct Evento *event);
+    void DefineDate(struct Cita *date);
+    void addEventDog(Dog*&lista, struct Evento *event,Dog perro);
+    void addDateDog(Dog*&lista ,struct Cita *date,Dog perro);
+};
+
+//--------------------Funciones----------------------//
+// Constructor
+Dog::Dog(){
+    this->Nombre = "GoldenIsaac";
+    this->Raza = "Golden Retriever";
+    this->Dia = 1;
+    this->Mes = 1;
+    this->Anio = 2024;
+    this->Peso = 5.2;
+    this->Padecimientos = "Ninguno";
+    this->event=nullptr;
+    this->date=nullptr;
+    this->next=nullptr;
+    this->prev=nullptr;
+}
+
+Dog *Dog::createNodo(string name, string raza, int dia, int mes , int anio, float peso, string padecimientos,Texture2D avatar){
+    Dog *nodo = new(Dog);
+    
+    nodo->Nombre = name;
+    nodo->Raza=raza;
+    
+    nodo->Dia=dia;
+    nodo->Mes=mes;
+    nodo->Anio=anio;
+    
+    nodo->Peso=peso;
+    nodo->Padecimientos=padecimientos;
+
+    nodo->Avatar=avatar;
+
+    nodo->event=nullptr;
+    nodo->next=nullptr;
+    nodo->prev=nullptr;
+    
+    return nodo;
+}
+
+void Dog::add(Dog *&lista, string name, string raza, int dia, int mes , int anio, float peso, string padecimientos,Texture2D avatar){
+    Dog *nodo = createNodo(name,raza,dia,mes,anio,peso,padecimientos,avatar);
+
+    if(lista==nullptr){
+        lista=nodo;
+    }
+    else{
+        Dog *temp = lista;
+        while(temp->next != nullptr){
+            temp=temp->next;
+        }
+        nodo->prev=temp;
+        temp->next=nodo;
+        
+    }
+}
+
+Dog::Dog(string name, string raza, int dia, int mes, int anio,float peso, string padecimientos){
+    this->Nombre = name;
+    this->Raza = raza;
+    this->Dia = dia;
+    this->Mes = mes;
+    this->Anio = anio;
+    this->Peso = peso;
+    this->Padecimientos = padecimientos;
+}
+
+void Dog::addEventDog(Dog*&lista, struct Evento *event,Dog perro){
+    Dog *temp = lista;
+    while (temp != nullptr) {
+        if (temp->Nombre == perro.Nombre) {
+            temp->event = event;
+            break;
+        }
+        temp = temp->next;
+    }
+
+    if (temp != nullptr && temp->event != nullptr) {
+        cout << temp->Nombre << ": " << temp->event->title << endl;
+    } else {
+        cout << "No se encontró el perro o el evento no se asignó correctamente." << endl;
+    }
+}
+
+void Dog::addDateDog(Dog*&lista ,struct Cita *date,Dog perro){
+    Dog *temp = lista;
+    while (temp != nullptr) {
+        if (temp->Nombre == perro.Nombre) {
+            temp->date = date;
+            break;
+        }
+        temp = temp->next;
+    }
+
+    if (temp != nullptr && temp->date != nullptr) {
+        cout << temp->Nombre << ": " << temp->date->titulo << endl;
+    } else {
+        cout << "No se encontró el perro o el evento no se asignó correctamente." << endl;
+    }
+}
+
+void Dog:: DefineEvents(struct Evento *event){
+    this->event=event;
+}
+
+void Dog:: DefineDate(struct Cita *date){
+    this->date=date;
+}
+
+// Gets
+string Dog::getName(){
+    return this->Nombre;
+}
+
+string Dog::getRaza(){
+    return this->Raza;
+}
+
+void Dog::DefineAvatar(Texture2D textura){
+    this->Avatar = textura;
+}
---
+++ src/functions/Images.h
@@ -0,0 +1,76 @@
+#include "raylib.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <utility> 
+
+typedef struct Cargas{
+    // INICIO
+    Texture2D FondoInicio;
+    Texture2D BotonEntrar;
+    // CREAR DUEÑO
+    Texture2D FondoCrearDueno;
+    Texture2D BotonListo;
+    Texture2D BotonAtras;
+    Texture2D Avatar1;
+    Texture2D Avatar2;
+    Texture2D Avatar3;
+    Texture2D Avatar4;
+    Texture2D Avatar5;
+    Texture2D Avatar6;
+    // MIS MASCOTAS
+    Texture2D FondoMisMascotas;
+    Texture2D BotonAnadir;
+    // AÑADIR MASCOTAS
+    Texture2D FondoAnadirMascotas;
+    Texture2D BotonPerro;
+    Texture2D BotonGato;
+    // REGISTRAR PERRO
+    Texture2D FondoRegPerro;
+    Texture2D FondoAvatarPerro;
+    Texture2D FondoAvatarGato;
+    Texture2D BotonAdelante;
+    Texture2D PERRO1;
+    Texture2D PERRO2;
+    Texture2D PERRO3;
+    Texture2D PERRO4;
+    Texture2D PERRO5;
+    Texture2D PERRO6;
+    Texture2D PERRO7;
+    Texture2D PERRO8;
+    // REGISTRAR GATO
+    Texture2D FondoRegGato;
+    Texture2D BotonMas;
+    Texture2D GATO1;
+    Texture2D GATO2;
+    Texture2D GATO3;
+    Texture2D GATO4;
+    Texture2D GATO5;
+    Texture2D GATO6;
+    Texture2D GATO7;
+    Texture2D GATO8;
+    // PERFIL MASCOTA
+    Texture2D FondoMiMascota;
+    Texture2D BotonMiInfo;
+    Texture2D BotonCartilla;
+    Texture2D BotonCalendario;
+    // CALENDARIO
+    Texture2D FondoCalendario;
+    Texture2D BotonAtrasAzul;
+    Texture2D BotonAdelanteAzul;
+    
+    // Fondo predeteminado
+    Texture2D Background;
+    // Posicion predeteminada
+    Vector2 Position = { 0.0f, 0.0f };
+    // Fuente
+    Font fuente;
+    // Color de los bloques de texto
+    Color Bloque1={245,246,243,255};
+} Cargas;
